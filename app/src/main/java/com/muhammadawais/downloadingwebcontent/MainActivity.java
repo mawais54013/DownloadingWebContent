e399da4940b3f9361b92180c78c755274da53c7f
MAwais <mawais54013@outlook.com>
2019-09-18 19:32:52 -0700

    Example Of Getting HTML Code

--- app/src/main/java/com/muhammadawais/downloadingwebcontent/MainActivity.java
+++ app/src/main/java/com/muhammadawais/downloadingwebcontent/MainActivity.java
@@ -6,16 +6,43 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
         @Override
-        protected String doInBackground(String... strings) {
+        protected String doInBackground(String... urls) {
 
-            Log.i("URL", strings[0]);
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
 
-            return "Done, Received Information";
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while(data != -1)
+                {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+                return "Failed";
+            }
+//            return "Success";
         }
     }
 
@@ -25,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         DownloadTask task = new DownloadTask();
-        String result;
+        String result = null;
 
         try {
             result = task.execute("https://www.google.com/").get();
@@ -33,5 +60,7 @@ public class MainActivity extends AppCompatActivity {
         catch (Exception e){
             e.printStackTrace();
         }
+
+        Log.i("Result", result);
     }
 }
